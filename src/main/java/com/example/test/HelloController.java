d4f2cbeee15198487576a601c90da98883532125
Administrator <admin@example.com>
2023-08-28 14:29:51 +0800

    HelloWorld!!

--- src/main/java/com/example/test/HelloController.java
+++ src/main/java/com/example/test/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
 
